523ca839be197af7863d127aef89363886a043d9
AgnieszkaGabka <94054240+AgnieszkaGabka@users.noreply.github.com>
2021-12-17 19:54:31 +0100

    Add files via upload
    Code changed to english in accordance with the instruction in the task description

--- 2001.py
+++ 2001.py
@@ -1,79 +1,79 @@
 import random
 import math
 
-def rzuty1(): # tura gracza - dwa rzuty, każdy po wciśnięciu enter, wynik dodawany do listy punkty_gracza i zwracany jako suma_punktów
-    punkty_gracza = []
+def rolls1(): # tura gracza - dwa rzuty, każdy po wciśnięciu enter, wynik dodawany do listy punkty_gracza i zwracany jako suma_punktów
+    gamers_points = []
     for _ in range(2):
-        text = input("Naciśnij enter, aby rzucić kostką")
+        text = input("Press enter to roll the dice")
         if text == "":
             x = random.randint(1,7)
-            punkty_gracza.append(x)
-            suma_punktow = sum(punkty_gracza)
+            gamers_points.append(x)
+            points_sum = sum(gamers_points)
         else:
-            print("you typed some text before pressing enter")
-    print(f"Masz obecnie {suma_punktow} punktów")
-    return(suma_punktow)
+            print("You typed some text before pressing enter")
+    print(f"You have now {points_sum} points")
+    return(points_sum)
 
-def rzuty2(): # tura komputera - dwa rzuty, automatyczne losowanie numerów, dodawanych do listy punkty_komp
-    punkty_komp = []
+def rolls2(): # tura komputera - dwa rzuty, automatyczne losowanie numerów, dodawanych do listy punkty_komp
+    comp_points = []
     for _ in range(2):
         x = random.randint(1,7)
-        punkty_komp.append(x)
-        suma_punktow = sum(punkty_komp)
-    print(f"Punkty komputera: {suma_punktow}")
-    return(suma_punktow)
+        comp_points.append(x)
+        points_sum = sum(comp_points)
+    print(f"Computer's points: {points_sum}")
+    return(points_sum)
 
 
-def gra(): #funkcja wywołująca wymagane podczas gry polecenia
-    lista_gracz = []  #lista kolejnych losowań gracza
-    lista_komp = [] #lista kolejnych losowań komputera
-    b = rzuty1() #wywołanie pierwszej tury gracza
-    lista_gracz.append(b) #dodanie sumy wyników pierwszej tury gracza do listy
-    c = rzuty2() #wywołanie pierwszej tury komputera
-    lista_komp.append(c) #dodanie sumy wyników pierwszej tury komputera do listy
-    x = sum(lista_gracz) #sumowanie wyników gracza
-    y = sum(lista_komp) #sumowanie wynik≤óœ komputera
+def game(): #funkcja wywołująca wymagane podczas gry polecenia
+    gamers_list = []  #lista kolejnych losowań gracza
+    comp_list = [] #lista kolejnych losowań komputera
+    b = rolls1() #wywołanie pierwszej tury gracza
+    gamers_list.append(b) #dodanie sumy wyników pierwszej tury gracza do listy
+    c = rolls2() #wywołanie pierwszej tury komputera
+    comp_list.append(c) #dodanie sumy wyników pierwszej tury komputera do listy
+    x = sum(gamers_list) #sumowanie wyników gracza
+    y = sum(comp_list) #sumowanie wynik≤óœ komputera
     while x < 2001 or y < 2001:
-        d = rzuty1() #wywołanie drugiej i kolejnych tur gracza
+        d = rolls1() #wywołanie drugiej i kolejnych tur gracza
         if d == 7:
             a = math.floor(x / 7)
             x = a #jeśli wylosowana zostanie siódemka, podziel sumę punktów gracza przez 7 i zamień punkty w liście na wynik dzielenia
-            lista_gracz.clear()
-            lista_gracz.append(x)
+            gamers_list.clear()
+            gamers_list.append(x)
         elif d == 11:
             a = x * 11
             x = a #jeśli wylosowana zostanie 11, pomnóż sumę punktów razy 11 i zamień punkty w liście na wynik mnożenia
-            lista_gracz.clear()
-            lista_gracz.append(x)
+            gamers_list.clear()
+            gamers_list.append(x)
         else:
-            lista_gracz.append(d) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
-            x = sum(lista_gracz)
-        print(f"Masz obecnie {x} punktów")
-        e = rzuty2() #wywołanie drugiej i kolejnych tur komputera
+            gamers_list.append(d) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
+            x = sum(gamers_list)
+        print(f"You have now {x} points")
+        e = rolls2() #wywołanie drugiej i kolejnych tur komputera
         if e == 7:
             a = math.floor(y / 7)
             y = a #jeśli wylosowana zostanie siódemka, podziel sumę punktów gracza przez 7 i zamień punkty w liście na wynik dzielenia
-            lista_komp.clear()
-            lista_komp.append(y)
+            comp_list.clear()
+            comp_list.append(y)
         elif e == 11:
             a = y * 11
             y = a #jeśli wylosowana zostanie 11, pomnóż sumę punktów razy 11 i zamień punkty w liście na wynik mnożenia
-            lista_komp.clear()
-            lista_komp.append(y)
+            comp_list.clear()
+            comp_list.append(y)
         else:
-            lista_komp.append(e) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
-            y = sum(lista_komp)
-        print(f"Komputer ma obecnie {y} punktów")
+            comp_list.append(e) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
+            y = sum(comp_list)
+        print(f"Computer now has {y} points")
         if x == 2001:
-            return "Wygrałeś! Masz 2001 punktów!" #koniec gry gdy gracz wylosuje w sumie 2001 punktów
+            return "You won! You have 2001 points!" #koniec gry gdy gracz wylosuje w sumie 2001 punktów
         elif y == 2001:
-            return "Komputer wygrał, ma 2001 punktów!" #koniec gry gdy komputer wylosuje w sumie 2001 punktów
+            return "The computer won, it has 2001 points!" #koniec gry gdy komputer wylosuje w sumie 2001 punktów
         elif x > 2001:
-            return "Gra przerwana, masz ponad 2001 punktów" #koniec gry, gdy gracz przekroczy sumę 2001 punktów
+            return "Game aborted, you have over 2001 points" #koniec gry, gdy gracz przekroczy sumę 2001 punktów
         elif y > 2001:
-            return "Gra przerwana, komputer ma ponad 2001 punktów" #koniec gry, gdy komputer przekroczy sumę 2001 punktów
+            return "Game aborted, the computer has over 2001 points" #koniec gry, gdy komputer przekroczy sumę 2001 punktów
         else:
             pass
 
 
-print(gra())
+print(game())
--- Modyfikacja 1.py
+++ Modyfikacja 1.py
@@ -1,136 +1,136 @@
 import random
 import math
 
-scianki = [3, 4, 6, 8, 10, 12, 20, 100]
-def wybor():
-    wybor = input("Wybierz dwie kości z zestawu: D3, D4, D6, D8, D10, D12, D20, D100. \
-    Swój wybór wpisz w formie: aDbD, gdzie a to liczba ścianek pierwszej kości, a b to liczba ścianek drugiej kości ")
-    rzuty = list(wybor)
-    return(rzuty) #funkcja zwraca ciąg znaków wpisanych przez gracza w formie listy
+walls = [3, 4, 6, 8, 10, 12, 20, 100]
+def choice():
+    choice = input("Choose two dice from the set: D3, D4, D6, D8, D10, D12, D20, D100. \
+    Enter your choice in the form: aDbD, where a is a number of walls of the first dice, and b is the number of walls of the second dice")
+    rolls = list(choice)
+    return(rolls) #funkcja zwraca ciąg znaków wpisanych przez gracza w formie listy
 
-def spacje(): #funckja usuwa spacje z listy, jeśli użytkownik je wpisał
-    dane = wybor()
-    dane = [x.strip(' ') for x in dane]
-    delete = [ele for ele in dane if ele.strip()]
+def spaces(): #funckja usuwa spacje z listy, jeśli użytkownik je wpisał
+    data = choice()
+    data = [x.strip(' ') for x in data]
+    delete = [ele for ele in data if ele.strip()]
     return (delete)
 
 def merge(): #funckja łączy liczby, jeśli wpisane zostały w formie znaków, np. z pozycji na liście "1", "0", "0" tworzy pozycję "100"
-    dane = spacje()
+    data = spaces()
     index = 1
-    while index < len(dane):
-        if dane[index].isdigit() and dane[index - 1].isdigit():
-            dane[index - 1] += dane.pop(index)
+    while index < len(data):
+        if data[index].isdigit() and data[index - 1].isdigit():
+            data[index - 1] += data.pop(index)
         else:
             index += 1
-    return (dane)
+    return (data)
 
 def convert(): #funckja zamienia liczby wpisane przez użytkownika na formę int i zwraca wszystkie znaki w formie listy, np. [2, "D", 3, "D"]
-    newdane = merge()
-    for i in range(0, len(newdane)):
+    newdata = merge()
+    for i in range(0, len(newdata)):
         try:
-            newdane[i] = int(newdane[i])
+            newdata[i] = int(newdata[i])
         except (ValueError, TypeError):
             pass
-    return (newdane)
+    return (newdata)
 
-def rzuty1(): #rzuty gracza - pobieranie danych funkcji convert
-    punkty_gracza = []
+def rolls1(): #rzuty gracza - pobieranie danych funkcji convert
+    gamers_points = []
     dane = convert()
-    text = input("Naciśnij enter, aby rzucić kostką")
+    text = input("Press enter to roll the dice")
     if text == "": #po wciśnięciu przez gracza enter funkcja zwraca lsowo wybran liczby z zakresu wprowadzonego przez gracza
         for index, i in enumerate(dane):
             if index == 0:
-                if i in scianki:
+                if i in walls:
                     x = random.randint(1, i)
-                    punkty_gracza.append(x) # dodwanie losowej liczby do listy z łącznnymi punktami
+                    gamers_points.append(x) # dodwanie losowej liczby do listy z łącznnymi punktami
                 else:
-                    print("Podaj poprawne dane") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
+                    print("Enter correct data") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
                     break
             if index == 2:
-                if i in scianki:
+                if i in walls:
                     x = random.randint(1, i)
-                    punkty_gracza.append(x) # dodwanie losowej liczby do listy z łącznnymi punktami - drugi rzut kostką w turze
+                    gamers_points.append(x) # dodwanie losowej liczby do listy z łącznnymi punktami - drugi rzut kostką w turze
                 else:
-                    print("Podaj poprawne dane") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
+                    print("Enter correct data") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
                     break
             if index == 1:
                 if i == "D": #sprawdzanie, czy gracz wpisał odpowiedni ciąg znaków
                     pass
                 else:
-                    print("Podaj poprawne dane") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
+                    print("Enter correct data") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
                     break
             if index == 3:
                 if i == "D": #sprawdzanie, czy gracz wpisał odpowiedni ciąg znaków
                     pass
                 else:
-                    print("Podaj poprawne dane") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
+                    print("Enter correct data") #jeśli gracz coś wpisze, zamiast wcisnąć enter, program zwraca informację o niepoprawnych danych
                     break
     else:
-        print("you typed some text before pressing enter")
-    suma_punktow = sum(punkty_gracza) #sumowanie punktów gracza nal iście
-    print(f"Masz obecnie {suma_punktow} punktów")
-    return (suma_punktow)
+        print("You typed some text before pressing enter")
+    points_sum = sum(gamers_points) #sumowanie punktów gracza nal iście
+    print(f"You now have {points_sum} points")
+    return (points_sum)
 
-def rzuty2(): #pierwsza tura komputera - dwa rzuty, automatyczne losowanie numerów, dodawanych do listy punkty_komp
-    punkty_komp = []
+def rolls2(): #pierwsza tura komputera - dwa rzuty, automatyczne losowanie numerów, dodawanych do listy punkty_komp
+    comps_points = []
     for _ in range(2):
-        a = random.choice(scianki)
+        a = random.choice(walls)
         x = random.randint(1, a)
-        punkty_komp.append(x)
-    suma_punktow = sum(punkty_komp)
-    print(f"Punkty komputera: {suma_punktow}")
-    return(suma_punktow)
+        comps_points.append(x)
+    points_sum = sum(comps_points)
+    print(f"Computer's points: {points_sum}")
+    return(points_sum)
 
-def gra(): #funkcja wywołująca wymagane podczas gry polecenia
-    lista_gracz = []  #lista kolejnych losowań gracza
-    lista_komp = [] #lista kolejnych losowań komputera
-    b = rzuty1() #wywołanie pierwszej tury gracza
-    lista_gracz.append(b) #dodanie sumy wyników pierwszej tury gracza do listy
-    c = rzuty2() #wywołanie pierwszej tury komputera
-    lista_komp.append(c) #dodanie sumy wyników pierwszej tury komputera do listy
-    x = sum(lista_gracz) #sumowanie wyników gracza
-    y = sum(lista_komp) #sumowanie wynik≤óœ komputera
+def game(): #funkcja wywołująca wymagane podczas gry polecenia
+    gamers_list = []  #lista kolejnych losowań gracza
+    comp_list = [] #lista kolejnych losowań komputera
+    b = rolls1() #wywołanie pierwszej tury gracza
+    gamers_list.append(b) #dodanie sumy wyników pierwszej tury gracza do listy
+    c = rolls2() #wywołanie pierwszej tury komputera
+    comp_list.append(c) #dodanie sumy wyników pierwszej tury komputera do listy
+    x = sum(gamers_list) #sumowanie wyników gracza
+    y = sum(comp_list) #sumowanie wynik≤óœ komputera
     while x < 2001 or y < 2001:
-        d = rzuty1() #wywołanie drugiej i kolejnych tur gracza
+        d = rolls1() #wywołanie drugiej i kolejnych tur gracza
         if d == 7:
             a = math.floor(x / 7)
             x = a #jeśli wylosowana zostanie siódemka, podziel sumę punktów gracza przez 7 i zamień punkty w liście na wynik dzielenia
-            lista_gracz.clear()
-            lista_gracz.append(x)
+            gamers_list.clear()
+            gamers_list.append(x)
         elif d == 11:
             a = x * 11
             x = a #jeśli wylosowana zostanie 11, pomnóż sumę punktów razy 11 i zamień punkty w liście na wynik mnożenia
-            lista_gracz.clear()
-            lista_gracz.append(x)
+            gamers_list.clear()
+            gamers_list.append(x)
         else:
-            lista_gracz.append(d) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
-            x = sum(lista_gracz)
-        print(f"Masz obecnie {x} punktów")
-        e = rzuty2() #wywołanie drugiej i kolejnych tur komputera
+            gamers_list.append(d) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
+            x = sum(gamers_list)
+        print(f"You have {x} points")
+        e = rolls2() #wywołanie drugiej i kolejnych tur komputera
         if e == 7:
             a = math.floor(y / 7)
             y = a #jeśli wylosowana zostanie siódemka, podziel sumę punktów gracza przez 7 i zamień punkty w liście na wynik dzielenia
-            lista_komp.clear()
-            lista_komp.append(y)
+            comp_list.clear()
+            comp_list.append(y)
         elif e == 11:
             a = y * 11
             y = a #jeśli wylosowana zostanie 11, pomnóż sumę punktów razy 11 i zamień punkty w liście na wynik mnożenia
-            lista_komp.clear()
-            lista_komp.append(y)
+            comp_list.clear()
+            comp_list.append(y)
         else:
-            lista_komp.append(e) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
-            y = sum(lista_komp)
-        print(f"Komputer ma obecnie {y} punktów")
+            comp_list.append(e) #jeśli nie będzie 7 albo 11, dodaj wynik tury do listy z punktami
+            y = sum(comp_list)
+        print(f"Computer now has {y} points")
         if x == 2001:
-            return "Wygrałeś! Masz 2001 punktów!" #koniec gry gdy gracz wylosuje w sumie 2001 punktów
+            return "You won! You have 2001 points!" #koniec gry gdy gracz wylosuje w sumie 2001 punktów
         elif y == 2001:
-            return "Komputer wygrał, ma 2001 punktów!" #koniec gry gdy komputer wylosuje w sumie 2001 punktów
+            return "The computer won, it has 2001 points!" #koniec gry gdy komputer wylosuje w sumie 2001 punktów
         elif x > 2001:
-            return "Gra przerwana, masz ponad 2001 punktów" #koniec gry, gdy gracz przekroczy sumę 2001 punktów
+            return "ame aborted, you have over 2001 points" #koniec gry, gdy gracz przekroczy sumę 2001 punktów
         elif y > 2001:
-            return "Gra przerwana, komputer ma ponad 2001 punktów" #koniec gry, gdy komputer przekroczy sumę 2001 punktów
+            return "Game aborted, the computer has over 2001 points" #koniec gry, gdy komputer przekroczy sumę 2001 punktów
         else:
             pass
 
 
-print(gra())
+print(game())
